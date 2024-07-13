becd09424ad69f39e324b4aa36285896bb6cb4df
polina-py <polina.bnp@gmail.com>
2024-07-13 11:21:32 +0300

    main

--- Py_Core/goit-algo-hw-05/task_1/main.py
+++ Py_Core/goit-algo-hw-05/task_1/main.py
@@ -1,2 +1,23 @@
-def caching_fibonacci():
+from typing import Callable
+
+def caching_fibonacci() -> Callable[[int], int]:
+    cache = {}
     
+    def fibonacci(n: int) -> dict.keys:
+        if n in cache[n]:
+            return cache[n]
+        else:
+            if n <= 0:
+                n = 0
+            elif n == 1:
+                n = 1
+            else:
+                n = n        
+
+        cache[n] = fibonacci(n-1) + fibonacci(n-2)
+        return cache[n]
+    
+    return fibonacci
+
+fibonacci_result = caching_fibonacci()
+print(fibonacci_result)
