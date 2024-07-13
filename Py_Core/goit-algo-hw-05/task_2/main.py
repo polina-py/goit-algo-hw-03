f6b9b789c43892d72ee1a26d42b1ba5e4b3c0ca1
polina-py <polina.bnp@gmail.com>
2024-07-13 12:54:05 +0300

    main

---
+++ Py_Core/goit-algo-hw-05/task_2/main.py
@@ -0,0 +1,15 @@
+import re
+from typing import Generator, Callable
+
+def generator_numbers(text: str) -> Generator[float, None, None]:
+    numbers = re.findall("\-?\d+", text)
+    for num in numbers:
+        yield float(num)
+
+def sum_profit(text: str, func: Callable[[str], Generator[float, None, None]]) -> float:
+    total_income = sum(func(text))
+    return total_income
+
+text = "Word 12 0 Second Word 45 -3 5.7 ?"
+total_income = sum_profit(text, generator_numbers)
+print(f"Загальний дохід: {total_income}")
